aeb2a5545ed4b930bef5b1306c867806633b9cb3
Doug <doug.hayden@onixnet.com>
2022-11-09 21:14:36 -0500

    include renamed folders

---
+++ test/main.py
@@ -0,0 +1,9 @@
+import re
+import json
+from datetime import datetime
+
+
+def this_is_a_fake_function(requests):
+    if 1 == 1:
+        pass
+    return 'done.'
---
+++ test2/main.py
@@ -0,0 +1,7 @@
+import re
+import json
+from datetime import datetime
+
+
+def not_a_real_function(requests):
+    return 'done.'
