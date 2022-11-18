be7114521e79772b3af0932c0edee031e7fcc134
Doug <doug.hayden@onixnet.com>
2022-11-18 18:56:16 -0500

    final version completing my test for SR

--- main.tf
+++ main.tf
@@ -29,5 +29,5 @@ module "service-accounts" {
   looker_clients = var.looker_clients
   # gcs_buckets_map     = module.storage.gcs_buckets_out # takes output from the srorage module to pass as a variable to the service-accounts module
   # sa_looker_email_map = module.service-accounts.sa_looker_email_out
-  depends_on = [module.storage]
+  # depends_on = [module.storage]
 }
