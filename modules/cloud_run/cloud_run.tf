3226d1f2ca763e023c9bf149e23a15d86872a997
Doug <doug.hayden@onixnet.com>
2023-01-06 21:30:06 -0500

    add file checksum build trigger

--- modules/cloud_run/cloud_run.tf
+++ modules/cloud_run/cloud_run.tf
@@ -32,6 +32,9 @@ resource "docker_image" "test-cloud-run" {
   build {
     context = "${path.cwd}/modules/cloud_run/test-cloud-run"
   }
+  triggers = {
+    dir_sha1 = sha1(join("", [for f in fileset(path.module, "test-cloud-run/*") : filesha1(f)]))
+  }
 }
 
 resource "docker_registry_image" "test-cloud-run" {
