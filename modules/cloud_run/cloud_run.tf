6bd7317bd48effb3467811dbc2ff5dd4c001b086
Doug <doug.hayden@onixnet.com>
2023-01-31 19:30:40 -0500

    formatting and add comments

--- modules/cloud_run/cloud_run.tf
+++ modules/cloud_run/cloud_run.tf
@@ -1,48 +1,3 @@
-# terraform {
-#   required_providers {
-#     docker = {
-#       source  = "kreuzwerker/docker"
-#       version = "3.0.1"
-#     }
-#   }
-# }
-
-# Make these test services public to allUsers
-
-data "google_iam_policy" "noauth-0" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-data "google_iam_policy" "noauth-1" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth-0" {
-  location    = google_cloud_run_service.test-cloud-run-0.location
-  project     = google_cloud_run_service.test-cloud-run-0.project
-  service     = google_cloud_run_service.test-cloud-run-0.name
-
-  policy_data = data.google_iam_policy.noauth-0.policy_data
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth-1" {
-  location    = google_cloud_run_service.test-cloud-run-1.location
-  project     = google_cloud_run_service.test-cloud-run-1.project
-  service     = google_cloud_run_service.test-cloud-run-1.name
-
-  policy_data = data.google_iam_policy.noauth-1.policy_data
-}
-
 resource "google_cloud_run_service" "test-cloud-run-0" {
   name     = "test-cloud-run-0"
   project  = var.project
@@ -52,7 +7,7 @@ resource "google_cloud_run_service" "test-cloud-run-0" {
     spec {
       service_account_name = "136454482223-compute@developer.gserviceaccount.com"
       containers {
-        image = "gcr.io/${var.project}/cloudrun/test-cloud-run-0:latest"
+        image = "gcr.io/${var.project}/cloudrun/test-cloud-run-0"
 
         env {
           name  = "PROJECT_ID"
@@ -77,7 +32,7 @@ resource "google_cloud_run_service" "test-cloud-run-1" {
     spec {
       service_account_name = "136454482223-compute@developer.gserviceaccount.com"
       containers {
-        image = "gcr.io/${var.project}/cloudrun/test-cloud-run-1:latest"
+        image = "gcr.io/${var.project}/cloudrun/test-cloud-run-1"
 
         env {
           name  = "PROJECT_ID"
@@ -93,6 +48,54 @@ resource "google_cloud_run_service" "test-cloud-run-1" {
   }
 }
 
+# Make the test cloud run services public to allUsers
+# TODO - use loops to consolidate
+
+data "google_iam_policy" "noauth-0" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers"
+    ]
+  }
+}
+
+data "google_iam_policy" "noauth-1" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers"
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth-0" {
+  location    = google_cloud_run_service.test-cloud-run-0.location
+  project     = google_cloud_run_service.test-cloud-run-0.project
+  service     = google_cloud_run_service.test-cloud-run-0.name
+  policy_data = data.google_iam_policy.noauth-0.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth-1" {
+  location    = google_cloud_run_service.test-cloud-run-1.location
+  project     = google_cloud_run_service.test-cloud-run-1.project
+  service     = google_cloud_run_service.test-cloud-run-1.name
+  policy_data = data.google_iam_policy.noauth-1.policy_data
+}
+
+# ### #
+# not using the kruezwerker provider for this experiment
+# ### #
+
+# terraform {
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "3.0.1"
+#     }
+#   }
+# }
+
 # resource "docker_image" "test-cloud-run" {
 #   name = "gcr.io/${var.project}/cloudrun/test-cloud-run:${var.image_tag}"
 #   build {
@@ -101,9 +104,6 @@ resource "google_cloud_run_service" "test-cloud-run-1" {
 #   triggers = {
 #     dir_sha1 = sha1(join("", [for f in fileset(path.module, "test-cloud-run/*") : filesha1("${path.module}/${f}")]))
 #   }
-#   # lifecycle {
-#   #   ignore_changes = [build]
-#   # }
 # }
 
 # ^^^ Ref: https://stackoverflow.com/questions/51138667/can-terraform-watch-a-directory-for-changes
@@ -122,9 +122,3 @@ resource "google_cloud_run_service" "test-cloud-run-1" {
 #     config_file = pathexpand("${path.cwd}/.docker/config.json")
 #   }
 # }
-
-# ### #
-#
-# test to see if this causes any changes in tf plan
-#
-# ### #
